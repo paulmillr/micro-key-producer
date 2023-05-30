4a8e242c04454b019e96f594632849b40e9b60b2
sshmatrix <19473027+sshmatrix@users.noreply.github.com>
2023-05-31 02:29:24 +0530

    base36 fix & added comments
    * natspec + minor re-factoring
    
    * linted + formatted
    
    * formatted with prettier
    
    * sync and stash
    
    * compact comments & code
    
    * reinstate ?question
    
    * remove extra at top
    
    * Use scure-base for base36
    
    * Forgot to format
    
    ---------
    
    Co-authored-by: 0xc0de4c0ffee <43116881+0xc0de4c0ffee@users.noreply.github.com>

--- src/ipns.ts
+++ src/ipns.ts
@@ -1,46 +1,57 @@
 import { ed25519 } from '@noble/curves/ed25519';
-import { hex, base32 } from '@scure/base';
+import { hex, base32, utils } from '@scure/base';
 import { concatBytes } from 'micro-packed';
 
-const ADDRESS_VERSION = new Uint8Array([0x03]);
-//const NAMESPACE = new Uint8Array([0xe5]);
+const base36 = utils.chain(
+  utils.radix(36),
+  utils.alphabet('0123456789abcdefghijklmnopqrstuvwxyz'),
+  utils.padding(0),
+  utils.join('')
+);
 
+// Formats IPNS public key in bytes array format to 'ipns://k...' string format
 export function formatPublicKey(pubBytes: Uint8Array) {
-  return `ipns://k${BigInt(`0x${hex.encode(pubBytes)}`).toString(36)}`;
+  return `ipns://k${base36.encode(pubBytes)}`;
 }
 
+// Takes an IPNS pubkey (address) string as input and returns bytes array of the key
+// Supports various formats ('ipns://k', 'ipns://b', 'ipns://f')
+// Handles decoding and validation of the key before returning pubkey bytes
 export function parseAddress(address: string): Uint8Array {
-  if (address.startsWith('ipns://')) address = address.slice(7);
   address = address.toLowerCase();
+  if (address.startsWith('ipns://')) address = address.slice(7);
   let hexKey;
   if (address.startsWith('k')) {
-    const b36 = '0123456789abcdefghijklmnopqrstuvwxyz';
-    let result = 0n;
-    for (let i = 1; i < address.length; ) {
-      result = result * 36n + BigInt(b36.indexOf(address.charAt(i++)));
-    }
-    hexKey = result.toString(16).padStart(80, '0');
+    // Decode base-36 pubkey (after removing 'k' prefix) and encode it as a hex string
+    hexKey = hex.encode(base36.decode(address.slice(1)));
   } else if (address.startsWith('b')) {
+    // Decode base-32 pubkey (after removing 'b' prefix) and encode it as a hex string
     hexKey = hex.encode(base32.decode(address.slice(1).toUpperCase()));
   } else if (address.startsWith('f')) {
     hexKey = address.slice(1);
-  } else throw new Error('Unsupported Base-X Format');
+  } else throw new Error('Unsupported Base-X Format'); // Throw error if pubkey format is not supported
 
-  if (hexKey.startsWith('0172002408011220') && hexKey.length == 80) return hex.decode(hexKey);
+  // Check if hexKey has expected prefix '0172002408011220' and length of 80
+  if (hexKey.startsWith('0172002408011220') && hexKey.length === 80) {
+    return hex.decode(hexKey);
+  }
+  // Throw error if IPNS key prefix is invalid
   throw new Error('Invalid IPNS Key Prefix: ' + hexKey);
 }
 
+// Generates an ed25519 pubkey from a seed and converts it to several IPNS pubkey formats
 export async function getKeys(seed: Uint8Array) {
   //? privKey "seed" should be checked for <ed25519.curve.n?
   if (seed.length != 32) throw new TypeError('Seed must be 32 bytes in length');
+  // Generate ed25519 public key from seed
   const pubKey = await ed25519.getPublicKey(seed);
-  // https://github.com/multiformats/multicodec/blob/master/table.csv
-  // 0x01 = v1, 0x72 = libp2p-key, ?0x00240801 = ?ed25519 key/type, 0x12 = ?sha256, 0x20 = 32 bytes length
+  // Create public key bytes by concatenating prefix bytes and pubKey
   const pubKeyBytes = concatBytes(
     new Uint8Array([0x01, 0x72, 0x00, 0x24, 0x08, 0x01, 0x12, 0x20]),
     pubKey
   );
   const hexKey = hex.encode(pubKeyBytes).toLowerCase();
+  // Return different representations of the keys
   return {
     publicKey: `0x${hexKey}`,
     privateKey: `0x${hex.encode(
